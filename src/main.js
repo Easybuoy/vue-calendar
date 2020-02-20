de1e1e934a2b698aea3f962ed0cb1537fb3e5c8e
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-02-20 17:09:21 +0100

    setup firebase configuration

--- src/main.js
+++ src/main.js
@@ -1,10 +1,40 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import VueTextAreaAutoSize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import dotenv from 'dotenv';
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+dotenv.config();
+
+const {
+  VUE_APP_FIREBASE_API_KEY,
+  VUE_APP_FIREBASE_AUTH_DOMAIN,
+  VUE_APP_FIREBASE_DATABASE_URL,
+  VUE_APP_FIREBASE_PROJECT_ID,
+  VUE_APP_FIREBASE_STORAGE_BUCKET,
+  VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  VUE_APP_FIREBASE_APP_ID
+} = process.env;
+
+firebase.initializeApp({
+  apiKey: VUE_APP_FIREBASE_API_KEY,
+  authDomain: VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VUE_APP_FIREBASE_APP_ID
+});
+
+export const db = firebase.firestore();
+
+Vue.use(VueTextAreaAutoSize);
+
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
